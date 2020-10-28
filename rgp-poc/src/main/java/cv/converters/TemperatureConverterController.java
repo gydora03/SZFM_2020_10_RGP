e201482387c3d84838beebc6633b4240d699f2c5
György Dóra <gyorgydora99@gmail.com>
2020-10-28 16:33:44 +0100

    Implemented the switchToWeightConverter method in TemperatureConverterController class

--- rgp-poc/src/main/java/cv/converters/TemperatureConverterController.java
+++ rgp-poc/src/main/java/cv/converters/TemperatureConverterController.java
@@ -75,5 +75,15 @@ public class TemperatureConverterController {
         CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
-
+    @FXML
+    public void switchToWeightConverter(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/weightConverter.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Weight Converter");
+        stage.setScene(scene);
+        stage.show();
+        CalculatorApplication.stage.getScene().setRoot(parent);
+    }
+    
 }
