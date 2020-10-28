0a1be31df4116a99dc8e0155f61bb7599be19288
György Dóra <gyorgydora99@gmail.com>
2020-10-28 16:23:10 +0100

    Implemented the switchToAdvancedCalculator method in WeightConverterController class

--- rgp-poc/src/main/java/cv/converters/WeightConverterController.java
+++ rgp-poc/src/main/java/cv/converters/WeightConverterController.java
@@ -31,4 +31,15 @@ public class WeightConverterController {
         CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
+    @FXML
+    public void switchToAdvancedCalculator(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/advancedCalculator.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Advanced Calculator");
+        stage.setScene(scene);
+        stage.show();
+        CalculatorApplication.stage.getScene().setRoot(parent);
+    }
+
 }
