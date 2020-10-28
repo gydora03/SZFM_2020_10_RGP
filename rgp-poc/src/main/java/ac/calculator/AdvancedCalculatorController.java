d512dcb9e3fe942462bc8c09ce085e82e7d7aa81
György Dóra <gyorgydora99@gmail.com>
2020-10-28 15:29:49 +0100

    Implemented the switchTobasicCalculator method in AdvancedCalculatorController class

--- rgp-poc/src/main/java/ac/calculator/AdvancedCalculatorController.java
+++ rgp-poc/src/main/java/ac/calculator/AdvancedCalculatorController.java
@@ -1,4 +1,34 @@
 package ac.calculator;
 
+import app.CalculatorApplication;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AdvancedCalculatorController {
+
+    @FXML
+    public void handleClickOnCloseMenuItem(ActionEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    @FXML
+    public void switchToBasicCalculator(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/basicCalculator.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Basic Calculator");
+        stage.setScene(scene);
+        stage.show();
+        CalculatorApplication.stage.getScene().setRoot(parent);
+    }
+
 }
