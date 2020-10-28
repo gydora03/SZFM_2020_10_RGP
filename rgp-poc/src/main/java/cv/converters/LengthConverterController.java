1f088e96f0b38807f9ceef8190c7894a281e70cc
György Dóra <gyorgydora99@gmail.com>
2020-10-28 16:14:20 +0100

    Implemented the switchToLengthConverter method in LengthConverterController class

--- rgp-poc/src/main/java/cv/converters/LengthConverterController.java
+++ rgp-poc/src/main/java/cv/converters/LengthConverterController.java
@@ -64,6 +64,17 @@ public class LengthConverterController {
         CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
+    @FXML
+    public void switchToLengthConverter(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/lengthConverter.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Length Converter");
+        stage.setScene(scene);
+        stage.show();
+        CalculatorApplication.stage.getScene().setRoot(parent);
+    }
+
 
 
 }
