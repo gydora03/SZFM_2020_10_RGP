ee7222ae69da16f77a270b7bb796042fd2a2c6bf
György Dóra <gyorgydora99@gmail.com>
2020-10-28 16:00:44 +0100

    Implemented the switchToBasicCalculator method in VolumeConverterController class

--- rgp-poc/src/main/java/cv/converters/VolumeConverterController.java
+++ rgp-poc/src/main/java/cv/converters/VolumeConverterController.java
@@ -1,8 +1,16 @@
 package cv.converters;
 
+import app.CalculatorApplication;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class VolumeConverterController {
 
@@ -11,4 +19,16 @@ public class VolumeConverterController {
         Platform.exit();
         System.exit(0);
     }
+
+    @FXML
+    public void switchToBasicCalculator(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/basicCalculator.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Basic Calculator");
+        stage.setScene(scene);
+        stage.show();
+        CalculatorApplication.stage.getScene().setRoot(parent);
+    }
+
 }
