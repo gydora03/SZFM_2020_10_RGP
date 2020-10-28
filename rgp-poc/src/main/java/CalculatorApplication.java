221b449febf4112a8b9c24b20134cb22b58420f4
György Dóra <gyorgydora99@gmail.com>
2020-10-28 13:27:32 +0100

    Implemented the CalculatorApplication class

--- rgp-poc/src/main/java/CalculatorApplication.java
+++ rgp-poc/src/main/java/CalculatorApplication.java
@@ -1,2 +1,19 @@
-public class CalculatorApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CalculatorApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent parent = FXMLLoader.load(CalculatorApplication.class.getResource("/fxml/basicCalculator.fxml"));
+        stage.setTitle("Basic Calculator");
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
 }
