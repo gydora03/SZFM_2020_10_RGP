7a4124cabc00578f99ac3bbc37b8ef16b9f1f122
György Dóra <gyorgydora99@gmail.com>
2020-10-28 15:32:14 +0100

    Implemented the switchToWeightConverter method in AdvancedCalculatorController class

--- rgp-poc/src/main/java/ac/calculator/AdvancedCalculatorController.java
+++ rgp-poc/src/main/java/ac/calculator/AdvancedCalculatorController.java
@@ -64,4 +64,16 @@ public class AdvancedCalculatorController {
         CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
+    @FXML
+    public void switchToWeightConverter(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/weightConverter.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Weight Converter");
+        stage.setScene(scene);
+        stage.show();
+        CalculatorApplication.stage.getScene().setRoot(parent);
+    }
+
+
 }
