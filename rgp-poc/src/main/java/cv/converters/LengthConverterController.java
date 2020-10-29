45e8f3e893c50e96e5d99546620eeb708223b621
György Dóra <gyorgydora99@gmail.com>
2020-10-29 13:43:01 +0100

    Fixed bugs and made LengthCovnerterController class switchable with all scenes

--- rgp-poc/src/main/java/cv/converters/LengthConverterController.java
+++ rgp-poc/src/main/java/cv/converters/LengthConverterController.java
@@ -1,18 +1,20 @@
 package cv.converters;
 
-import app.CalculatorApplication;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LengthConverterController {
+
+    @FXML
+    MenuBar menuBar;
 
     @FXML
     public void handleClickOnCloseMenuItem(ActionEvent event) {
@@ -24,77 +26,61 @@ public class LengthConverterController {
     public void switchToBasicCalculator(ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/basicCalculator.fxml"));
         Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) menuBar.getScene().getWindow();
         stage.setTitle("Basic Calculator");
         stage.setScene(scene);
         stage.show();
-        CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
     @FXML
     public void switchToAdvancedCalculator(ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/advancedCalculator.fxml"));
         Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) menuBar.getScene().getWindow();
         stage.setTitle("Advanced Calculator");
         stage.setScene(scene);
         stage.show();
-        CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
     @FXML
     public void switchToPICalculator(ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/piCalculator.fxml"));
         Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage)  menuBar.getScene().getWindow();
         stage.setTitle("PI Calculator");
         stage.setScene(scene);
         stage.show();
-        CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
     @FXML
     public void switchToVolumeConverter(ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/volumeConverter.fxml"));
         Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) menuBar.getScene().getWindow();
         stage.setTitle("Volume Converter");
         stage.setScene(scene);
         stage.show();
-        CalculatorApplication.stage.getScene().setRoot(parent);
-    }
-
-    @FXML
-    public void switchToLengthConverter(ActionEvent event) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/lengthConverter.fxml"));
-        Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("Length Converter");
-        stage.setScene(scene);
-        stage.show();
-        CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
     @FXML
     public void switchToWeightConverter(ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/weightConverter.fxml"));
         Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) menuBar.getScene().getWindow();
         stage.setTitle("Weight Converter");
         stage.setScene(scene);
         stage.show();
-        CalculatorApplication.stage.getScene().setRoot(parent);
     }
 
     @FXML
     public void switchToTemperatureConverter(ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/temperatureConverter.fxml"));
         Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) menuBar.getScene().getWindow();
         stage.setTitle("Temperature Converter");
         stage.setScene(scene);
         stage.show();
-        CalculatorApplication.stage.getScene().setRoot(parent);
     }
+
 
 }
