c9db2c30312da649ab099cdab35a902bec2f079b
Racs Tamás <cant0r.universal@gmail.com>
2020-11-01 09:43:43 +0100

    Added class for Chudnovsky approximation

---
+++ rgp-poc/src/main/java/pi/approximators/ChudnovskyApproximator.java
@@ -0,0 +1,13 @@
+package pi.approximators;
+
+public class ChudnovskyApproximator implements IApproximator{
+    @Override
+    public String approximate(int precision) {
+        return null;
+    }
+
+    @Override
+    public String approximate(int precision, int from, int until) {
+        return approximate(precision);
+    }
+}
