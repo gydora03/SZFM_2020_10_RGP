307503000bb2b168f13cbbdbffa2a68e35257d96
Racs Tamás <cant0r.universal@gmail.com>
2020-11-01 10:18:55 +0100

    Implemented the serial version of Zeta approximation for the demo program

--- rgp-poc/src/main/java/pi/approximators/ZetaApproximator.java
+++ rgp-poc/src/main/java/pi/approximators/ZetaApproximator.java
@@ -1,9 +1,21 @@
 package pi.approximators;
 
+import org.apfloat.Apfloat;
+import org.apfloat.ApfloatMath;
+
 public class ZetaApproximator implements IApproximator{
     @Override
     public String approximate(int precision) {
-        return null;
+        Apfloat sum = Apfloat.ZERO;
+        int floatPrecision = getFloatPrecision();
+        for(int i = 0; i < precision*2; i++) {
+            sum = sum.add(Apfloat.ONE.
+                    divide(ApfloatMath
+                            .pow(new Apfloat(i,floatPrecision),
+                                    new Apfloat(2, floatPrecision))));
+        }
+        sum = ApfloatMath.sqrt(sum.multiply(new Apfloat(6,floatPrecision)));
+        return sum.toString(true);
     }
 
     @Override
