5006488e02299450c541fd86f91f22cde02e61fe
Racs Tamás <cant0r.universal@gmail.com>
2020-11-01 12:12:40 +0100

    Fixed formula implementation

--- rgp-poc/src/main/java/pi/approximators/LeibnizApproximator.java
+++ rgp-poc/src/main/java/pi/approximators/LeibnizApproximator.java
@@ -10,10 +10,13 @@ public class LeibnizApproximator implements IApproximator {
         Apfloat sum = new Apfloat(0, floatPrecision);
 
         for (int i = 1; i < precision*2; i++) {
-            sum = sum.add(pi.divide(new Apfloat(String.valueOf(i), floatPrecision)
-                    .multiply(new Apfloat("2", floatPrecision)).add(Apfloat.ONE)));
+            sum = Apfloat.ONE.divide(new Apfloat(String.valueOf(i), floatPrecision)
+                    .multiply(new Apfloat("2", floatPrecision)).add(Apfloat.ONE));
+            if((i-1) % 2 == 0)
+                pi = pi.subtract(sum);
+            else
+                pi = pi.add(sum);
         }
-        pi = pi.subtract(sum);
         pi = pi.multiply(new Apfloat("4", floatPrecision));
         return pi.toString(true);
 
