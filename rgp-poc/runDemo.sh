00c2f77d1846a4d62b19ee726477178bd67ddfae
Racs Tamás <cant0r.universal@gmail.com>
2020-11-01 09:01:31 +0100

    Updated script with executing the db initialization script

--- rgp-poc/runDemo.sh
+++ rgp-poc/runDemo.sh
@@ -1,9 +1,11 @@
 echo "Starting demo application"
 echo "Setting up in-memory H2 database"
 mvn exec:java@console > mvn_h2_log.txt &
+sleep 0.5
+mvn exec:java@initdb > mvn_h2_init.txt &
 echo "Running rgp-poc"
 java -jar target/rgp-poc*.jar
 
-kill -9 `jobs -l | grep mvn | sed -r "s/\s\s+/ /g" | cut -d' ' -f 2`
+kill -9 `jobs -l | grep mvn | sed -r "s/\s\s+/ /g" | cut -d' ' -f 2` 2> /dev/null
 
 echo "Bye"
