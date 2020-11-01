94691b4776d4c2cbccfd4adb5557a74133e0df14
Racs Tamás <cant0r.universal@gmail.com>
2020-11-01 09:28:41 +0100

    Updated startup scripts

--- rgp-poc/runDemo.sh
+++ rgp-poc/runDemo.sh
@@ -1,7 +1,9 @@
+echo "(Re)building application"
+mvn clean package
 echo "Starting demo application"
 echo "Setting up in-memory H2 database"
 mvn exec:java@console > mvn_h2_log.txt &
-sleep 0.5
+sleep 0.2
 mvn exec:java@initdb > mvn_h2_init.txt &
 echo "Running rgp-poc"
 java -jar target/rgp-poc*.jar
