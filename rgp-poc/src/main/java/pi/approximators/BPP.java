686a410309fa3f768a49f197219b5e351ee9b0a7
Racs Tamás <cant0r.universal@gmail.com>
2020-11-14 22:21:44 +0100

    Parallized the BBP algorithm

--- rgp-poc/src/main/java/pi/approximators/BPP.java
+++ rgp-poc/src/main/java/pi/approximators/BPP.java
@@ -1,12 +1,24 @@
 package pi.approximators;
 
+import com.sun.glass.ui.Application;
 import org.apfloat.Apfloat;
 import org.apfloat.ApfloatMath;
 import org.apfloat.Apint;
 import org.apfloat.ApintMath;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class BPP implements IApproximator {
     private int precision = getFloatPrecision();
+    Apfloat d1 = null, d4 = null, d5 = null, d6 = null;
+    List<Future<Apfloat>> ds = null;
+
+    private ExecutorService execService;
+
     @Override
     public String approximate(int precision) {
         return approximate(precision, 0, 1000001);
@@ -15,13 +27,29 @@ public class BPP implements IApproximator {
     @Override
     public String approximate(int precision, int n, int limit) {
         StringBuilder hexLetters = new StringBuilder();
-        this.precision = Math.max(getFloatPrecision(),precision);
-        for(int i=n; i < limit; i++)
-        {
-            Apfloat d1 = calculateSubFormula(1, i);
-            Apfloat d4 = calculateSubFormula(4, i);
-            Apfloat d5 = calculateSubFormula(5, i);
-            Apfloat d6 = calculateSubFormula(6, i);
+        this.precision = Math.max(getFloatPrecision(), precision);
+        execService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (int i = n; i < limit; i++) {
+
+            final int iter = i;
+            try {
+                ds = execService.invokeAll(List.of(
+                        () -> calculateSubFormula(1, iter),
+                        () -> calculateSubFormula(4, iter),
+                        () -> calculateSubFormula(5, iter),
+                        () -> calculateSubFormula(6, iter)
+
+                ));
+
+                d1 = ds.get(0).get();
+                d4 = ds.get(1).get();
+                d5 = ds.get(2).get();
+                d6 = ds.get(3).get();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+                System.exit(-6);
+            }
+
 
             Apfloat pi = (new Apfloat(4, this.precision).multiply(d1)).
                     subtract(new Apfloat(2, this.precision).multiply(d4))
@@ -68,7 +96,7 @@ public class BPP implements IApproximator {
         for (i = new Apint(0); i.compareTo(new Apint(d)) <= 0; i = i.add(Apint.ONE)) {
             exp = new Apint(d).subtract(i);
             mod = i.multiply(new Apint(8)).add(new Apint(formulaPartId));
-            result = result.add(expmod(base, exp, mod).divide(new Apfloat(mod.toString(),precision)));
+            result = result.add(expmod(base, exp, mod).divide(new Apfloat(mod.toString(), precision)));
             result = result.subtract(ApfloatMath.floor(result));
             System.out.println(i.toString(true));
         }
