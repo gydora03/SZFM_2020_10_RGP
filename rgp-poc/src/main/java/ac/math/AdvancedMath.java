e9d42e67580e95d2386368e3f5954767be98f1ba
Racs Tamás <cant0r.universal@gmail.com>
2020-11-15 18:21:45 +0100

    Adde basic skeleton im,plementation for AdvancedMath

---
+++ rgp-poc/src/main/java/ac/math/AdvancedMath.java
@@ -0,0 +1,76 @@
+package ac.math;
+
+import org.apfloat.Apfloat;
+
+public class AdvancedMath implements IMathFunctionality<Apfloat>{
+
+    @Override
+    public Apfloat add(Apfloat a, Apfloat b) {
+        return null;
+    }
+
+    @Override
+    public Apfloat subtract(Apfloat a, Apfloat b) {
+        return null;
+    }
+
+    @Override
+    public Apfloat multiply(Apfloat a, Apfloat b) {
+        return null;
+    }
+
+    @Override
+    public Apfloat divide(Apfloat a, Apfloat b) {
+        return null;
+    }
+
+    @Override
+    public Apfloat sin(Apfloat a) {
+        return null;
+    }
+
+    @Override
+    public Apfloat cos(Apfloat a) {
+        return null;
+    }
+
+    @Override
+    public Apfloat tan(Apfloat a) {
+        return null;
+    }
+
+    @Override
+    public Apfloat ctg(Apfloat a) {
+        return null;
+    }
+
+    @Override
+    public Apfloat mod(Apfloat a, Apfloat b) {
+        return null;
+    }
+
+    @Override
+    public Apfloat power(Apfloat a, Apfloat b) {
+        return null;
+    }
+
+    @Override
+    public Apfloat sqrt(Apfloat a) {
+        return null;
+    }
+
+    @Override
+    public Apfloat factorial(Apfloat n) {
+        return null;
+    }
+
+    @Override
+    public Apfloat gcd(Apfloat a, Apfloat b) {
+        return null;
+    }
+
+    @Override
+    public Apfloat lcm(Apfloat a, Apfloat b) {
+        return null;
+    }
+}
