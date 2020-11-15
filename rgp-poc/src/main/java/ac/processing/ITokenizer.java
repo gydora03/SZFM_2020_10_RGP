8cda0c56728e4dfd0790798f7524e609a6e175dc
Racs Tamás <cant0r.universal@gmail.com>
2020-11-15 20:43:48 +0100

    Now tokenize throws an exception when encountering invalid characters

--- rgp-poc/src/main/java/ac/processing/ITokenizer.java
+++ rgp-poc/src/main/java/ac/processing/ITokenizer.java
@@ -3,5 +3,5 @@ package ac.processing;
 import java.util.List;
 
 public interface ITokenizer {
-    public List<Token> tokenize(String input);
+    public List<Token> tokenize(String input) throws Exception;
 }
