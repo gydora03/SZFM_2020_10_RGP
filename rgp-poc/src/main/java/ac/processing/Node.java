1dc8904f61dc2454ba22197b9ab4669ba08bcfec
Racs Tamás <cant0r.universal@gmail.com>
2020-11-15 19:32:00 +0100

    Implemented the Node data class

--- rgp-poc/src/main/java/ac/processing/Node.java
+++ rgp-poc/src/main/java/ac/processing/Node.java
@@ -1,4 +1,30 @@
 package ac.processing;
 
 public class Node {
+    private final String data;
+    private Node leftChild, rightChild;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public void setLeftChild(Node n) {
+        leftChild = n;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setRightChild(Node n) {
+        rightChild = n;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
