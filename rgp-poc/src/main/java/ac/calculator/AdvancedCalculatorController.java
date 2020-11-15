870ca48dd981ae795629111d921089597366cb9f
Racs Tamás <cant0r.universal@gmail.com>
2020-11-15 18:36:00 +0100

    Added references to the controls of advancedCalculator.fxml

--- rgp-poc/src/main/java/ac/calculator/AdvancedCalculatorController.java
+++ rgp-poc/src/main/java/ac/calculator/AdvancedCalculatorController.java
@@ -6,12 +6,140 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AdvancedCalculatorController {
+
+    @FXML
+    private MenuItem piMenuItem;
+
+    @FXML
+    private MenuItem eulerMenuItem;
+
+    @FXML
+    private MenuItem BernsteinMenuItem;
+
+    @FXML
+    private MenuItem goldenRatioMenuItem;
+
+    @FXML
+    private TextField display;
+
+    @FXML
+    private Button clear;
+
+    @FXML
+    private Button allClear;
+
+    @FXML
+    private Button addition;
+
+    @FXML
+    private Button subtraction;
+
+    @FXML
+    private Button multiplication;
+
+    @FXML
+    private Button division;
+
+    @FXML
+    private Button percentage;
+
+    @FXML
+    private Button sinus;
+
+    @FXML
+    private Button cosinus;
+
+    @FXML
+    private Button tangens;
+
+    @FXML
+    private Button cotangens;
+
+    @FXML
+    private Button modulo;
+
+    @FXML
+    private Button xPowY;
+
+    @FXML
+    private Button squareRoot;
+
+    @FXML
+    private Button greatestCommonDivisor;
+
+    @FXML
+    private Button leastCommonMultiple;
+
+    @FXML
+    private Button factorial;
+
+    @FXML
+    private Button equal;
+
+    @FXML
+    private Button zero;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button decimalDot;
+
+    @FXML
+    private Button plusMinus;
+
+    @FXML
+    private Button memoryOne;
+
+    @FXML
+    private Button memoryTwo;
+
+    @FXML
+    private Button memoryThree;
+
+    @FXML
+    private Button memoryFour;
+
+    @FXML
+    private MenuItem closeMenuItem;
+
+    private AdvancedCalculator advancedCalculator;
+    private String memory_one = "";
+    private String memory_two = "";
+    private String memory_three = "";
+    private String memory_four = "";
 
     @FXML
     MenuBar menuBar;
