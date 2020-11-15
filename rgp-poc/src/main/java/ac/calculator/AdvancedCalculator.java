024fdf7541aa67a938ef631bdea692c1fee64580
Racs Tamás <cant0r.universal@gmail.com>
2020-11-15 20:43:05 +0100

    Added the ability to return the underlying math implementation

--- rgp-poc/src/main/java/ac/calculator/AdvancedCalculator.java
+++ rgp-poc/src/main/java/ac/calculator/AdvancedCalculator.java
@@ -44,5 +44,9 @@ public class AdvancedCalculator implements ICalculator<Apfloat> {
     public void clearCalculations() {
         this.expression = null;
     }
+
+    public IMathFunctionality getMathImplementation() {
+        return bm;
+    }
     
 }
