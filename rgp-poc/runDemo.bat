267df93afa177dd5762210dd3d2258de8e1989ee
György Dóra <gyorgydora99@gmail.com>
2020-11-15 16:09:28 +0100

    Some changes in runDemo batch file

--- rgp-poc/runDemo.bat
+++ rgp-poc/runDemo.bat
@@ -2,8 +2,8 @@ echo "(Re)building application"
 call mvn package
 echo "Starting demo application"
 echo "Setting up in-memory H2 database"
-start "mvn exec:java@console > mvn_h2_log.txt"
-start "mvn exec:java@initdb > mvn_h2_init.txt"
+start mvn exec:java@console > mvn_h2_log.txt
+start mvn exec:java@initdb > mvn_h2_init.txt
 echo "Running rgp-poc"
 call java -jar target/rgp-poc-1.0-SNAPSHOT.jar
 
