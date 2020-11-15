af425d1473766fd22a7fc160eecf0ba6a7cd0082
Racs Tamás <cant0r.universal@gmail.com>
2020-11-15 19:32:32 +0100

    Implemented the Token data class

--- rgp-poc/src/main/java/ac/processing/Token.java
+++ rgp-poc/src/main/java/ac/processing/Token.java
@@ -1,4 +1,19 @@
 package ac.processing;
 
 public class Token {
+    private final MathType type;
+    private final String value;
+
+    public Token(MathType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public MathType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
