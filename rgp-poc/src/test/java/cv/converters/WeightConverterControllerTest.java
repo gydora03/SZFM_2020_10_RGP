0b243034f449f66dab1e1fd211e5843100587d66
György Dóra <gyorgydora99@gmail.com>
2020-11-27 17:22:21 +0100

    Created unit test class for WeightConverter

---
+++ rgp-poc/src/test/java/cv/converters/WeightConverterControllerTest.java
@@ -0,0 +1,16 @@
+package cv.converters;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeightConverterControllerTest {
+
+    @Test
+    void testConvertUnitToGram() {
+    }
+
+    @Test
+    void testConvertToTargetUnit() {
+    }
+}
