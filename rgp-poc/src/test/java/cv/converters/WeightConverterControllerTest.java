4764891deed52ab75e55be510bbdc8f9f36d2e4a
György Dóra <gyorgydora99@gmail.com>
2020-11-27 18:20:29 +0100

    Implemented convertUnitToGram junit test to WeightConverter

--- rgp-poc/src/test/java/cv/converters/WeightConverterControllerTest.java
+++ rgp-poc/src/test/java/cv/converters/WeightConverterControllerTest.java
@@ -8,6 +8,9 @@ class WeightConverterControllerTest {
 
     @Test
     void testConvertUnitToGram() {
+        assertNotEquals(20, new WeightConverterController().convertUnitToGram("decagram", 685));
+        assertNotEquals(34, new WeightConverterController().convertUnitToGram("pounds", 6));
+        assertEquals(0.05, new WeightConverterController().convertUnitToGram("milligram", 50));
     }
 
     @Test
