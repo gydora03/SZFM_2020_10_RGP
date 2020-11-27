530d9672eef552d2224cf812442ff54a3933a851
György Dóra <gyorgydora99@gmail.com>
2020-11-27 17:48:37 +0100

    Created unit test class for BasicMath

---
+++ rgp-poc/src/test/java/bc/providers/BasicMathTest.java
@@ -0,0 +1,28 @@
+package bc.providers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicMathTest {
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void testSubtract() {
+    }
+
+    @Test
+    void testMultiply() {
+    }
+
+    @Test
+    void testDivide() {
+    }
+
+    @Test
+    void testMod() {
+    }
+}
