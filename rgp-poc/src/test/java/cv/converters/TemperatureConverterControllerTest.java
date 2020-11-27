6382bb2943d54793ddeb6889b360a0fb86a7c006
György Dóra <gyorgydora99@gmail.com>
2020-11-27 18:27:20 +0100

    Created junit test for Temperature Converter

---
+++ rgp-poc/src/test/java/cv/converters/TemperatureConverterControllerTest.java
@@ -0,0 +1,16 @@
+package cv.converters;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterControllerTest {
+
+    @Test
+    void convertUnitToCelsius() {
+    }
+
+    @Test
+    void convertToTargetUnit() {
+    }
+}
