af8adf68692d0dea82628d7d931052e44cd1681b
György Dóra <gyorgydora99@gmail.com>
2020-11-27 17:21:55 +0100

    Created unit test class for AdvancedMath

---
+++ rgp-poc/src/test/java/ac/providers/AdvancedMathTest.java
@@ -0,0 +1,64 @@
+package ac.providers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdvancedMathTest {
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void testSubtract() {
+    }
+
+    @Test
+    void testMultiply() {
+    }
+
+    @Test
+    void testDivide() {
+    }
+
+    @Test
+    void testSin() {
+    }
+
+    @Test
+    void testCos() {
+    }
+
+    @Test
+    void testTan() {
+    }
+
+    @Test
+    void testCtg() {
+    }
+
+    @Test
+    void testMod() {
+    }
+
+    @Test
+    void testPower() {
+    }
+
+    @Test
+    void testSqrt() {
+    }
+
+    @Test
+    void testFactorial() {
+    }
+
+    @Test
+    void testGcd() {
+    }
+
+    @Test
+    void testLcm() {
+    }
+}
