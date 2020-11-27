b831c5ac63f60ba26fa2a4a5b1179ee76e53cc96
György Dóra <gyorgydora99@gmail.com>
2020-11-27 18:36:21 +0100

    Implemented convertUnitToCelsius junit test to Temperature Converter

--- rgp-poc/src/test/java/cv/converters/TemperatureConverterControllerTest.java
+++ rgp-poc/src/test/java/cv/converters/TemperatureConverterControllerTest.java
@@ -7,10 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class TemperatureConverterControllerTest {
 
     @Test
-    void convertUnitToCelsius() {
+    void testConvertUnitToCelsius() {
+        assertNotEquals(20, new TemperatureConverterController().convertUnitToCelsius("kelvin", 70));
+        assertNotEquals(34, new TemperatureConverterController().convertUnitToCelsius("fahrenheit", 6));
+        assertEquals(10, new TemperatureConverterController().convertUnitToCelsius("fahrenheit", 50));
     }
 
     @Test
-    void convertToTargetUnit() {
+    void testConvertToTargetUnit() {
     }
 }
